6a6cbead1deb3a41784b3372ad61f12d682b9330
AidaQ27 <aidaqueraltt@gmail.com>
2022-05-14 16:14:48 +0200

    Inicializando Junit Test

--- src/test/java/UD21/Calculadora/AppTest.java
+++ src/test/java/UD21/Calculadora/AppTest.java
@@ -1,5 +1,7 @@
 package UD21.Calculadora;
 
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,9 +14,48 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+	
+	Vista vista = new Vista();
+	Controller controller = new Controller(vista);
+	
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void testgetOper() {
+    	controller.setOperando1(0.0);
+    	double resultado = controller.getOperando1();
+    	double delta = 1;
+    	assertEquals(resultado, 0.0, delta);
+    	
+    	
+    }
+    
+    @Test
+    public void testgetOper2() {
+    	controller.setOperando2(0.0);
+    	double resultado = controller.getOperando2();
+    	double delta = 1;
+    	assertEquals(resultado, 0.0, delta);
+    	
+    	
+    }
+    
+    
+    
+    @Test
+    public void testSuma() {
+    	controller.suma();
+    	
+    
+    }
+    
+    
+    
+    
+    
+    
 }
